1990336c745d3ee0d3a4c7daacd06f04df3621a2
Coolrexx <74631959+Coolrexx@users.noreply.github.com>
2022-04-22 22:42:19 +1000

    thanks cody <3

--- src/main/java/com/coolrexx/beesechurger/common/registry/BeesechurgerItems.java
+++ src/main/java/com/coolrexx/beesechurger/common/registry/BeesechurgerItems.java
@@ -1,6 +1,9 @@
 package com.coolrexx.beesechurger.common.registry;
 
 import com.coolrexx.beesechurger.Beesechurger;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,5 +13,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class BeesechurgerItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Beesechurger.MOD_ID);
 
-    public static final RegistryObject<Item> BEESECHURGER = ITEMS.register("beesechurger", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(16).food(BeesechurgerFoods.BEESECHURGER)));
+    public static final RegistryObject<Item> BEESECHURGER = ITEMS.register("beesechurger", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(16).food(new FoodProperties.Builder().nutrition(8).saturationMod(10.2F).effect(new MobEffectInstance(BeesechurgerEffects.BEESECHURGERS_BLESSING.get(), 6000, 0), 1.0F).alwaysEat().build())));
 }
